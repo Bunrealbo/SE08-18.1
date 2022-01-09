be1a17e26e8a8e4c87127519c9d4da58b065ad93
anhquanhus0902 <pvaquans22@gmail.com>
2022-01-09 21:54:53 +0700

    request api get score data

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Score
+{
+
+    public string Id { get; set; }
+    public int Scorex { get; set; }
+    public int Level { get; set; }
+
+    public Score(string id, int score, int level)
+    {
+        this.Id = id;
+        this.Scorex = score;
+        this.Level = level;
+    }
+
+    public override string ToString()
+    {
+        return String.Format("Score[id={0}, score={1}, level={2}]", this.Id, this.Scorex, this.Level);
+    }
+}
